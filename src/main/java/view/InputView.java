8987384f27c3c56c75d6c7b9bfde18f4983a2b63
taek2222 <oot309@daum.net>
2024-12-10 14:45:01 +0900

    feat: 게임 참여 순서에 따라 배팅 금액 메시지 출력 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,24 @@
 package view;
 
 import java.util.Scanner;
+import validation.CommonValidator;
 
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public String readGamePlayer() {
+    public String readGamePlayers() {
         System.out.println("게임에 참여할 사람의 이름을 입렵하세요.(쉼표 기준으로 분리)");
         return SCANNER.nextLine();
+    }
+
+    public int readBettingMoney(String name) {
+        System.out.println();
+        System.out.printf("%s의 배팅 금액은?", name);
+        System.out.println();
+
+        String input = SCANNER.nextLine();
+        CommonValidator.validateIsNumeric(input);
+        return Integer.parseInt(input);
     }
 }
