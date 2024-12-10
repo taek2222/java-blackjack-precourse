99e5cfa7003df3b470e9110bec1e40818cb2fb9f
taek2222 <oot309@daum.net>
2024-12-10 14:45:22 +0900

    feat: 입력 파싱 후 플레이어 금액 저장 구현

--- src/main/java/controller/BlackjackController.java
+++ src/main/java/controller/BlackjackController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import domain.user.Player;
+import java.util.ArrayList;
+import java.util.List;
+import util.PlayerParser;
 import view.InputView;
 import view.OutputView;
 
@@ -14,6 +18,13 @@ public class BlackjackController {
     }
 
     public void run() {
-        String input = inputView.readGamePlayer();
+        String input = inputView.readGamePlayers();
+        List<String> players = PlayerParser.parserInputPlayers(input);
+
+        List<Player> players1 = new ArrayList<>();
+        for (String player : players) {
+            int money = inputView.readBettingMoney(player);
+            players1.add(new Player(player, money));
+        }
     }
 }
